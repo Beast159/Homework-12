635c2443a5f178de90df6582035f51dd5c0f4d6c
Алексей <Beast159@mail.ru>
2023-03-27 17:48:40 +0500

    HW12 готово

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,6 @@ public class Main {
         deliveryBankCard(150);
     }
 
-
     public static void checkIsLeapYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным");
